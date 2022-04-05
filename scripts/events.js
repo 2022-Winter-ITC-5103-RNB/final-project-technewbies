4bef38d87fc0be3cecf62d1838362b44d94115f7
Alvaro <beltranalvaro08@gmail.com>
2022-04-04 22:11:48 -0400

    Alvaro second phase

---
+++ scripts/events.js
@@ -0,0 +1,9 @@
+function modalOn() {
+  const modal = document.getElementById("modalFFF");
+  modal.classList.toggle("modal-active");
+}
+
+function modalOff() {
+  const modal = document.getElementById("modalFFF");
+  modal.classList.toggle("modal-active");
+}
