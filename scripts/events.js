4b9476b5c17594a74b33d56dfd61ef5b96b263ad
Alvaro Beltran Caceres <n01511461@humbermail.ca>
2022-04-04 23:40:40 -0400

    Alvaro Second Phase

--- scripts/events.js
+++ scripts/events.js
@@ -1,9 +1,42 @@
-function modalOn() {
-  const modal = document.getElementById("modalFFF");
+const data = [
+  {
+    id: "card1",
+    name: "Farm, Friends and Family",
+    content:
+      "FFF is an event held during the summer. Have a great time with your family while meeting our little furry friends. ",
+    price: 45,
+  },
+
+  {
+    id: "card2",
+    name: "Puppy Fest",
+    content:
+      "FFF is an event held during the summer. Have a great time with your family while meeting our little furry friends. ",
+    price: 45,
+  },
+];
+const event_btn = document.querySelectorAll("#event-btn");
+
+event_btn.forEach((e_btn) => {
+  e_btn.addEventListener("click", (btn) => {
+    let idElement = btn.target.parentElement.id;
+    for (let datas in data) {
+      if (idElement == data[datas].id) {
+        modalOn(data[datas]);
+      }
+    }
+  });
+});
+
+function modalOn({ name, content, price }) {
+  console.log("THIS IS DATA" + name);
+  const modal = document.getElementById("modal");
   modal.classList.toggle("modal-active");
+  let modalHeight = document.body.scrollHeight;
+  modal.style.height = `${modalHeight}px`;
 }
 
 function modalOff() {
-  const modal = document.getElementById("modalFFF");
+  const modal = document.getElementById("modal");
   modal.classList.toggle("modal-active");
 }
