e0948be2a69b4b748c820e3b1914249de8fd64a5
rayhan904 <rehan904@yahoo.com>
2022-04-19 12:13:59 -0400

    bug fixes

--- scripts/individual.js
+++ scripts/individual.js
@@ -1,4 +1,5 @@
-
+const url = document.querySelector(".hide").getAttribute("href");
+console.log(url);
 const showImage = (i) => {
-    window.open(`https://raw.githubusercontent.com/2022-Winter-ITC-5103-RNB/final-project-technewbies/master/images/portfolio${i}.jpg?token=GHSAT0AAAAAABTQCNUVSHSAG5H4JHGV2BY2YSV344Q`)
+    window.open(url)
 }
